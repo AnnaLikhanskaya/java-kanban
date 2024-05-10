3c16c872af5702944087795dbd4554822752baa3
AnnaLikhanskaya <a.lihansky@yandex.ru>
2024-05-10 18:15:36 +0300

    Добавлено логирование

--- src/service/InMemoryHistoryManager.java
+++ src/service/InMemoryHistoryManager.java
@@ -96,15 +96,6 @@ public class InMemoryHistoryManager implements HistoryManager {
             this.next = next;
             this.prev = prev;
         }
-
-        @Override
-        public String toString() {
-            return "Node{" +
-                    "item=" + item +
-                    ", next=" + next +
-                    ", prev=" + prev +
-                    '}';
-        }
     }
 }
 
